d842a029c735c49e319f0d4f2cd83fbcf90f78c7
Pablo Benedix <pabling.benedix@gmail.com>
2024-02-22 23:03:38 -0300

    Interfaz de menu completada

--- app/sections/menu.py
+++ app/sections/menu.py
@@ -6,26 +6,69 @@ def menu(page: ft.Page):
     page.title = "Menu principal"
     page.scroll = "adaptive"
 
-    card = ft.Card(
-            content=ft.Container(
-                content=ft.Column(
-                    [
-                        ft.ListTile(
-                            leading=ft.Icon(ft.icons.ALBUM),
-                            title=ft.Text("The Enchanted Nightingale"),
-                            subtitle=ft.Text(
-                                "Music by Julie Gable. Lyrics by Sidney Stein."
-                            ),
-                        ),
-                        ft.Row(
-                            [ft.TextButton("Buy tickets"), ft.TextButton("Listen")],
-                            alignment=ft.MainAxisAlignment.END,
-                        ),
-                    ]
+    def itemCard(icon, titleText, subtitleText, dataText):
+
+        cardInformation = ft.Row([
+            ft.Icon(icon),
+            ft.Column([
+                ft.Text(titleText, 
+                    theme_style=ft.TextThemeStyle.TITLE_LARGE,
+                    weight=ft.FontWeight.BOLD,
                 ),
-                width=400,
-                padding=10,
-            )
+                ft.Text(subtitleText, 
+                    theme_style=ft.TextThemeStyle.LABEL_LARGE,
+                    color=ft.colors.INDIGO_200    
+                ),
+            ], alignment = ft.MainAxisAlignment.CENTER, spacing=0.5)
+        ])
+
+        card = ft.Container(
+            content=(
+                ft.Row([
+                    cardInformation,
+                    ft.Container(
+                        content = ft.Text(
+                            dataText, 
+                            weight=ft.FontWeight.BOLD, 
+                            theme_style=ft.TextThemeStyle.TITLE_LARGE,
+                        ),
+                        bgcolor=ft.colors.INDIGO_800, 
+                        border_radius=10,
+                        width=80,
+                        height=50,
+                        alignment=ft.alignment.center,
+                    )
+                ], alignment = ft.MainAxisAlignment.SPACE_BETWEEN)
+            ),
+            margin=10,
+            padding=15,
+            alignment=ft.alignment.center,
+            bgcolor=ft.colors.INDIGO_500,
+            width=350,
+            height=100,
+            border_radius=10,
+        )
+        
+        return card
+
+
+
+    def column_with_alignment(align: ft.MainAxisAlignment):
+        return ft.Row(
+            [
+                ft.Container(
+                    content=ft.Column([
+                        ft.Text("Datos", 
+                            theme_style=ft.TextThemeStyle.DISPLAY_MEDIUM,
+                            text_align="CENTER"
+                        ),
+                        itemCard(ft.icons.ALBUM_OUTLINED, "Velocidad", "KM/H", "80"), 
+                        itemCard(ft.icons.SPEED, "Revoluciones", "Revoluciones por Minuto", "2350"),
+                        itemCard(ft.icons.AIR, "Temperatura", "Grados Celcius", "87"),
+                        itemCard(ft.icons.WATER_DROP_OUTLINED, "Consumo", "Litros por Hora", "5.2"),
+                    ], spacing=1),
+                ),
+            ], alignment=align,
         )
 
     # Crear instancia de la base de datos
@@ -35,9 +78,5 @@ def menu(page: ft.Page):
     db.close_connection()
 
     page.add(
-        ft.Text("MENU", 
-            theme_style=ft.TextThemeStyle.DISPLAY_MEDIUM,
-            text_align="CENTER",
-        ),
-        card
+        column_with_alignment(ft.MainAxisAlignment.CENTER),
     )
